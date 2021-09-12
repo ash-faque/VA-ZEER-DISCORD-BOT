6fdc5eb27dad7ce59efaf8d16507acf758eb6d2b
Ashfaque P P <60512006+ash-faque@users.noreply.github.com>
2021-09-12 23:27:12 +0530

    1458

---
+++ index.js
@@ -0,0 +1,164 @@
+require('dotenv').config();
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+const mail_id = process.env.mail;
+const pass = process.env.pass;
+
+const token = process.env.token;
+const clientId = process.env.clientId;
+const guildId = process.env.guildId;
+const gecwydian = process.env.gecwydian;
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+const { Client, Intents, MessageActionRow, MessageButton, MessageEmbed, MessageSelectMenu } = require('discord.js');
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
+
+const { REST } = require('@discordjs/rest');
+const rest = new REST({ version: '9' }).setToken(token);
+
+const { Routes } = require('discord-api-types/v9');
+
+const { SlashCommandBuilder, bold, italic, strikethrough, underscore, spoiler, quote, blockQuote  } = require('@discordjs/builders');
+
+const nodemailer = require("nodemailer");
+const transporter = nodemailer.createTransport({ service: 'gmail', auth: { user: mail_id, pass: pass } });
+
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+const CODES = {};
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+const mail = (destination, code) => {
+    let mailOptions = {
+        from: mail_id,
+        to: destination,
+        subject: 'CODE FOR DISCORD SERVER VERIFICATION',
+        html: `
+        <h2>👋 GREETINGS FROM VA-ZEER (BOT)</h2><br>
+        <h3>TO VARIFY THAT YOU ARE FROM GEC WAYANAD TYPE</h3>
+        <br><b style="background-color: green; color: yellow;"><h1>/got_code ${code}</h1><b><br>
+        <h3>IN ANY OF THE CHANNELS OF TINKERHUB GECW'S DISCORD SERVER.<h3>`
+    };
+	
+	return new Promise((reolve, reject) => {
+		transporter.sendMail(mailOptions, function (error, info) {
+			if (error) {
+				console.log(error);
+				reject('Failed to send mail 😐');
+			} else {
+				//console.log('Email sent: ' + info.response);
+				reolve('Mail with a verification code has been send succesfully 😃');
+			};
+		});
+	});
+};
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+const commands = [
+	new SlashCommandBuilder()
+		.setName('verify').setDescription('initialize verification as a gecwian through college mail id')
+		.addStringOption(option =>
+			option.setName('e-mail')
+				.setDescription('your mail-id on the gecwyd.ac.in domain')
+				.setRequired(true)),
+	new SlashCommandBuilder()
+	.setName('got_code').setDescription('to authenticate the entered mail id (after getting code in mail inbox)')
+	.addNumberOption(option =>
+		option.setName('code')
+			.setDescription("code after the '/got_code' from mail recieved")
+			.setRequired(true)),
+].map(command => command.toJSON());
+
+(async () => {
+	try {
+		await rest.put(
+			Routes.applicationGuildCommands(clientId, guildId),
+			{ body: commands },
+		);
+		console.log('Successfully registered application commands.');
+	} catch (error) {
+		console.error(error);
+	}
+})();
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+client.on('interactionCreate', async interaction => {
+	if (!interaction.isCommand()) return;
+
+	const { commandName } = interaction;
+
+	switch (commandName){
+		case 'verify':
+			let verifier_id = interaction.user.id;
+			let mailId = interaction.options._hoistedOptions[0].value;
+			if (mailId.endsWith('@gecwyd.ac.in')){
+				await interaction.deferReply();
+				let code = Math.floor((Math.random() * 9990) + 1010).toString();
+				CODES[verifier_id] = code;
+				mail(mailId, code).then(r => {
+					interaction.editReply({ 
+						content: r, 
+						ephemeral: true 
+					});
+				}).catch(e => {
+					interaction.editReply({ 
+						content: e, 
+						ephemeral: true 
+					});
+				});
+			} else {
+				interaction.reply({ 
+					content: 
+`🐱‍🚀 Mail ID should end with "@gecwyd.ac.in"
+${mailId} doesn't looks like so...`, 
+					ephemeral: true 
+				});
+			};
+			break;
+		case 'got_code':
+			let user_id = interaction.user.id;
+			let code = interaction.options._hoistedOptions[0].value;
+			if (CODES[user_id] == code){
+				interaction.reply({ 
+					content:
+`🎊🎉🎉🎊
+RESPECT + +
+WELCOME TO
+GECWIAN CLUB`, 
+					ephemeral: true 
+				});
+				let GECWIAN_ROLE = interaction.guild.roles.cache.find(r => r.id === gecwydian);
+				interaction.member.roles.add(GECWIAN_ROLE);
+			} else {
+				interaction.reply({ 
+					content: 
+`WOOPS..🤖
+THAT'S NOT WHAT I SEND.
+CONSIDER RETYPING.`, 
+					ephemeral: true 
+				});
+			};
+		default:
+			break;
+	};
+});
+
+
+/////////      	/verify e-mail:ashfaque_18m103me@gecwyd.ac.in 
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+client.once('ready', () => {
+	console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.login(process.env.token);
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
---
+++ notinuse.js
@@ -0,0 +1,48 @@
+		// case 'ping':
+		// 	await interaction.reply(`Ping: ${client.ws.ping} ms`);
+		// 	break;
+		// case 'server':
+		// 	await interaction.reply(`Server name: ${interaction.guild.name}\nTotal members: ${interaction.guild.memberCount}`);
+		// 	break;
+		// case 'user':
+		// 	await interaction.reply(`Your tag: ${interaction.user.tag}\nYour id: ${interaction.user.id}`);
+		// 	break;
+		// case 'button':
+		// 	const row = new MessageActionRow()
+		// 	.addComponents(
+		// 		new MessageSelectMenu()
+		// 			.setCustomId('select')
+		// 			.setPlaceholder('Nothing selected')
+		// 			.addOptions([
+		// 				{
+		// 					label: 'Select me',
+		// 					description: 'This is a description',
+		// 					value: 'first_option',
+		// 				},
+		// 				{
+		// 					label: 'You can select me too',
+		// 					description: 'This is also a description',
+		// 					value: 'second_option',
+		// 				},
+		// 			]),
+		// 	);
+		// 	const embed = new MessageEmbed()
+		// 	.setColor('#0099ff')
+		// 	.setTitle('Some title')
+		// 	.setURL('https://discord.js.org/')
+		// 	.setDescription('Some description here');
+		// 	await interaction.reply({ content: 'Pong!', ephemeral: true, embeds: [embed], components: [row] });
+		// 	break;
+
+
+
+
+
+
+
+
+
+        	// new SlashCommandBuilder().setName('ping').setDescription('Bots websocket connection ping.'),
+	// new SlashCommandBuilder().setName('server').setDescription('Server info.'),
+	// new SlashCommandBuilder().setName('user').setDescription('User info.'),
+	// new SlashCommandBuilder().setName('button').setDescription('Sends button'),
